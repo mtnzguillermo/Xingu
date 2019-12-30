371f45155d1aef2ccfeb9ddd8572efff18a44c53
Ana María Pérez Torres <Ana@MacBook-Pro-de-Ana-2.local>
2019-12-30 11:54:27 +0100

    DB Tables (Money, Loans, Expenses)

---
+++ DB/DataManagement.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+def InsertExpense(DB_Name, date, field, value, concept, observations):
+
+    ExpenseRoute = "DataBases/"
+
+    ExpenseConnection = sqlite3.connect(ExpenseRoute + DB_Name)
+    ExpenseCursor = connection.cursor()
+
+    # TODO: Calcular code
+    ExpenseCursor.execute("INSERT INTO EXPENSES VALUES(code, date, field, value, concept, observations)")
+
+    ExpenseConnection.commit()
+
+    ExpenseConnection.close()
+
+def InsertLoan(DB_Name, date, person, value, concept, observations):
+
+    LoanRoute = "DataBases/"
+
+    LoanConnection = sqlite3.connect(LoanRoute + DB_Name)
+    LoanCursor = connection.cursor()
+
+    # TODO: Calcular code
+    LoanCursor.execute("INSERT INTO LOANS VALUES(code, date, person, value, concept, observations)")
+
+    LoanConnection.commit()
+
+    LoanConnection.close()
