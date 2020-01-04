d3a2fd4e9a9ff513181f9265eb3020a9ec42a9f2
Ana María Pérez Torres <Ana@MacBook-Pro-de-Ana-2.local>
2020-01-04 14:49:32 +0100

    Write into DB

--- GUI/ExpenseWindow.py
+++ GUI/ExpenseWindow.py
@@ -24,9 +24,9 @@ class ExpenseWindow(XinguWindow):
         self.mode_field_list = ["Comida", "Hogar", "Transporte", "Ocio", "Ropa", "Viajes", "Caprichos", "Regalos", "Gasto mensual", "Ingreso mensual", "Ingreso puntual", "Otros"]
         self.mode_field = StringVar(self)
         self.mode_field.set(self.mode_field_list[0])
-        self.ModeOM = OptionMenu(self.ExpenseFrame, self.mode_field, *self.mode_field_list)
-        self.ModeOM.config(width=18, font=('Calibri', 14))
-        self.ModeOM.place(x=200,y=100)
+        self.ModeField = OptionMenu(self.ExpenseFrame, self.mode_field, *self.mode_field_list)
+        self.ModeField.config(width=18, font=('Calibri', 14))
+        self.ModeField.place(x=200,y=100)
 
         #Value
         self.ValueLabel = Label(self.ExpenseFrame, text="Valor:", bg="Purple", fg="White", font=("Calibri", 14))
@@ -43,17 +43,31 @@ class ExpenseWindow(XinguWindow):
         self.ConceptEntry.place(x=200, y=200)
 
         #Observations
-        self.ObservationslLabel = Label(self.ExpenseFrame, text="Observaciones:", bg="Purple", fg="White", font=("Calibri", 14))
-        self.ObservationslLabel.place(x=50, y=250)
+        self.ObservationsLabel = Label(self.ExpenseFrame, text="Observaciones:", bg="Purple", fg="White", font=("Calibri", 14))
+        self.ObservationsLabel.place(x=50, y=250)
         
-        self.ObservationslLabel = Text(self.ExpenseFrame, font=("Calibri", 14), height=3, width=20, xscrollcommand=TRUE)
-        self.ObservationslLabel.place(x=200, y=250)
+        self.ObservationsText = Text(self.ExpenseFrame, font=("Calibri", 14), height=3, width=20, xscrollcommand=TRUE)
+        self.ObservationsText.place(x=200, y=250)
 
         #Button
         self.OKButton = Button(self.ExpenseFrame, text="Introducir Gasto", font=("Calibri", 14), height=2, width=20)
         self.OKButton.bind("<Button-1>",self.Send)
         self.OKButton.place(x=225, y=360, anchor=N)
 
-    def Send(self):
-        pass
+    #Action of the button
+    def Send(self, event):
+        #pass
+        #imports
+        from DB.DataManagement import InsertExpense
+        from datetime import datetime
+
+        self.date = self.DateEntry.get()
+        self.datetime = datetime.strptime(self.date, '%d/%m/%y')
+
+        self.field = self.mode_field.get()
+        self.value = self.ValueEntry.get()
+        self.concept = self.ConceptEntry.get()
+        self.observations = self.ObservationsText.get("1.0",'end-1c')
+
+        InsertExpense("Prueba", self.datetime, self.field, self.value, self.concept, self.observations)
 
