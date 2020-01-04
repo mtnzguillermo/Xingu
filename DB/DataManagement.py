1869a595fc378f52a6055c95a7bac1d25ffdf690
Guillermo Martinez-Lopez <g15willy@gmail.com>
2020-01-04 17:02:26 +0100

    New DB configuration

--- DB/DataManagement.py
+++ DB/DataManagement.py
@@ -9,10 +9,16 @@ def InsertExpense(DB_Name, date, field, value, concept, observations):
 
     # TO DO: Calcular code
     code = 20200103001
+    virtual = 0.0
+    liquid = 0.0
+    month_indicator = 0.0
+    month_savings = 0.0
 
-    values = (code, date, field, value, concept, observations)
+    expense_values = (code, date, field, value, concept, observations)
+    money_values = (code, date, virtual, liquid, month_indicator, month_savings)
 
-    ExpenseCursor.execute("INSERT INTO EXPENSES VALUES(?, ?, ?, ?, ?, ?)", values)
+    ExpenseCursor.execute("INSERT INTO EXPENSES VALUES(?, ?, ?, ?, ?, ?)", expense_values)
+    ExpenseCursor.execute("INSERT INTO MONEY VALUES(?, ?, ?, ?)", money_values)
 
     ExpenseConnection.commit()
 
