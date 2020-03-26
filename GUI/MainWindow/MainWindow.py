26cefc12af9d9cc7c18b22c35fd2a57f930cf221
Guillermo Martinez-Lopez <g15willy@gmail.com>
2020-03-26 17:07:56 +0100

    Separation of DataFrame in a different module

--- GUI/MainWindow/MainWindow.py
+++ GUI/MainWindow/MainWindow.py
@@ -4,8 +4,10 @@ from GUI.ExpenseWindow import *
 from GUI.IncomeWindow import IncomeWindow
 from GUI.LoanWindow import LoanWindow
 from DB.DataManagement import *
+from GUI.MainWindow.DataFrame import DataFrame
 
-import tkinter.ttk as ttk
+#import tkinter.ttk as ttk
+
 
 class MainWindow(XinguWindow):
 
@@ -25,7 +27,6 @@ class MainWindow(XinguWindow):
         self.OptFrame = OptionsFrame(self)
         
                 
-
 class OptionsFrame(Frame):
 
     def __init__(self, root_window):
@@ -145,97 +146,3 @@ class OptionsFrame(Frame):
 
     def NewLoanWindow(self, event):
         self.loan_window = LoanWindow(self.root_window)
-
-class DataFrame(Frame):
-
-    def __init__(self, root_window):
-        super().__init__(root_window, bg="Yellow", width=725, height=700)
-
-        # Introduction of the frame in MainWindow
-        self.place(x=0, y=75)
-
-        self.root_window = root_window
-
-        #root_window.OptFrame.Visualize()
-
-        self.title = Label(self, text= "Mes y Año", fg="White", background="Purple", font=("Calibri", 16))
-
-        self.headers = ["Fecha", "Campo", "Valor", "Concepto", "Total", "Indicador", "Ahorro"]
-
-        self._tree = ttk.Treeview(self, height=25, columns=self.headers, show="headings")
-
-        self.title.pack(side=TOP, fill="x")
-
-        # Scrollbars 
-        vsb = ttk.Scrollbar(self, orient="vertical", command=self._tree.yview)
-        vsb.pack(side='right', fill='y')
-        #hsb = ttk.Scrollbar(self, orient="horizontal", command=self._tree.xview)
-        #hsb.pack(side='bottom', fill='x')
-        #Not sure if hsb is necessary 
-
-        #self._tree.configure(xscrollcommand=hsb.set, yscrollcommand=vsb.set)
-        self._tree.configure(yscrollcommand=vsb.set)
-        self._tree.pack(side="left")
-
-        for header in self.headers:
-            self._tree.heading(header, text=header.title())
-            self._tree.column(header, stretch=False, width=101)
-        
-        self._tree.bind("<Double-1>", self.DataDoubleClick)
-        
-    def VisualizeMonthMode(self, year, month_string, month_integer):
-
-        # Table (Monthly)
-
-        self.title.config(text = month_string + " " + str(year))
-
-        #for header in self.headers:
-        #    self._tree.heading(header, text=header.title())
-        #    self._tree.column(header, stretch=False, width=101)
-
-        for i in self._tree.get_children():
-            self._tree.delete(i)
-
-        #Get parameters from DB
-        self.visualization_data = GetMonthExpenses(self.root_window.DB_Name, year, month_integer)
-
-        #cursor = [(str(self.contador), "Celda2"), ("Celda3", "Celda4")]
-        
-        self.index_tree = 0
-        for row in self.visualization_data:
-            self.index_tree  = self.index_tree + 1
-            self.add_row(row[1:], self.index_tree)
-    
-    def VisualizeYearMode(self):
-        pass
-
-    def add_row(self, row, index_tree):
-
-        self.item = 4
-        self._tree.insert('', 'end', id=index_tree, values=row)
-        for i, self.item in enumerate(row):
-            col_width = 101
-            if self._tree.column(self.headers[i], width=None) < col_width:
-                    self._tree.column(self.headers[i], width=col_width)
-
-    def DataDoubleClick(self, event):
-        self.item = self._tree.selection()[0] # now you got the item on that tree
-        #self.expense_window = ExpenseWindow(self.root_window)
-        print ("you clicked on " + self.item)
-
-        #self.item_int= self.item[1:]
-        #print("you clicked on (int) " + self.item_int)
-
-        #self.item_dec = int(self.item_int, 16)
-        #print("you clicked on (dec) " + str(self.item_dec))
-
-        #self.item_int = int(self.item[1:]) - 1 
-        #self.clicked_code = self.visualization_data[self.item_dec][0]
-
-        self.item_int = int(self.item) - 1
-        self.clicked_code = self.visualization_data[self.item_int][0]
-
-        self.EditExpenseWindow = EditExpenseWindow(self.root_window, self.clicked_code)
-
-        #print ("Data " + str(self.visualization_data[self.item_int]))
-        #print ("Code " + str(self.visualization_data[self.item_int][0]))
