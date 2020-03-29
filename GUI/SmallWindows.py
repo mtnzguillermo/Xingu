f46fd31d3e86340f19ff3a3eef486f3576c3923b
Ana María Pérez Torres <Ana@MacBook-Pro-de-Ana-2.local>
2020-03-29 13:20:58 +0200

    PasswordWindow implemented

--- GUI/SmallWindows.py
+++ GUI/SmallWindows.py
@@ -9,7 +9,8 @@ class ConfirmWindow(XinguWindow):
         self.caller_window = caller_window
         self.original_event = original_event
 
-        self.title("bla")
+        self.title("")
+        self.resizable(False, False)
         ConfigWindowFrame=Frame(self,bg="White",width=200,height=200)
         ConfigWindowFrame.pack() #fill="both",expand="True")
 
@@ -30,3 +31,37 @@ class ConfirmWindow(XinguWindow):
         self.caller_window.ConfirmedAction(self.original_event)
 
         #self.destroy()
+
+class PasswordWindow(XinguWindow):
+
+    def __init__(self, caller_window, original_event):
+        super().__init__()
+
+        self.caller_window = caller_window
+        self.original_event = original_event
+
+        self.title("")
+        self.resizable(False, False)
+        PassWindowFrame=Frame(self,bg="White",width=300,height=200)
+        PassWindowFrame.pack() #fill="both",expand="True")
+
+        #Password Label
+        PassLabel = Label(PassWindowFrame, text="Contraseña:", bg="White", fg="Purple", font=("Calibri", 14))
+        PassLabel.place(x=150, y=30, anchor=N)
+
+        #Password Entry
+        self.PasswordEntry = Entry(PassWindowFrame, font=("Calibri", 18), show="·", bd=2)
+        self.PasswordEntry.bind("<Return>", self.PassConfirm) 
+        self.PasswordEntry.place(x=150, y=70, anchor=N)
+
+        #Confirm Button
+        ConfirmButton = Button(PassWindowFrame, text="Confirmar", font=("Calibri", 14), height=2, width=10)
+        ConfirmButton.bind("<Button-1>",self.PassConfirm)
+        ConfirmButton.place(x=150, y=130, anchor=N)
+        
+        #self.destroy()
+
+    #Action of the Confirm Button
+    def PassConfirm(self, event):
+
+        pass
