18bdb403b6f7c4dd5b057f31d5164bb34072c9b2
Ana María Pérez Torres <Ana@MacBook-Pro-de-Ana-2.local>
2020-10-04 11:48:13 +0200

    Update ExpenseWindow (Add type and Field domains)

--- GUI/ExpenseWindow.py
+++ GUI/ExpenseWindow.py
@@ -11,7 +11,7 @@ class ExpenseWindow(XinguWindow):
         
         self.title("Nuevo Gasto")
         self.resizable(False, False)
-        self.ExpenseFrame=Frame(self,bg="Purple",width=450,height=450)
+        self.ExpenseFrame=Frame(self,bg="Purple",width=450,height=500)
         self.ExpenseFrame.pack() #fill="both",expand="True")
 
         #Date
@@ -22,45 +22,56 @@ class ExpenseWindow(XinguWindow):
         self.DateEntry.place(x=200, y=50)
 
         #Field
-        self.FieldLabel = Label(self.ExpenseFrame, text="Tipo:", bg="Purple", fg="White", font=("Calibri", 14))
+        self.FieldLabel = Label(self.ExpenseFrame, text="Campo:", bg="Purple", fg="White", font=("Calibri", 14))
         self.FieldLabel.place(x=50, y=100)
 
-        self.mode_field_list = ["Comida", "Hogar", "Transporte", "Ocio", "Salud", "Ropa", "Viajes", "Caprichos", "Regalos", "Mensual", "Otros"]
+        self.mode_field_list = ["Comida", "Hogar", "Transporte", "Ocio", "Salud", "Ropa", "Viajes", "Caprichos", "Regalos", "Otros"]
         self.mode_field = StringVar(self)
         self.mode_field.set(self.mode_field_list[0])
         self.ModeField = OptionMenu(self.ExpenseFrame, self.mode_field, *self.mode_field_list)
         self.ModeField.config(width=18, font=('Calibri', 14))
         self.ModeField.place(x=200,y=100)
 
+        #Type
+        self.TypeLabel = Label(self.ExpenseFrame, text="Tipo:", bg="Purple", fg="White", font=("Calibri", 14))
+        self.TypeLabel.place(x=50, y=150)
+
+        self.mode_type_list = ["Mensual", "Puntual"]
+        self.mode_type = StringVar(self)
+        self.mode_type.set(self.mode_type_list[0])
+        self.ModeType = OptionMenu(self.ExpenseFrame, self.mode_type, *self.mode_type_list)
+        self.ModeType.config(width=18, font=('Calibri', 14))
+        self.ModeType.place(x=200,y=150)
+
         #Value
         self.ValueLabel = Label(self.ExpenseFrame, text="Valor:", bg="Purple", fg="White", font=("Calibri", 14))
-        self.ValueLabel.place(x=50, y=150)
+        self.ValueLabel.place(x=50, y=200)
         
         self.ValueEntry = Entry(self.ExpenseFrame, font=("Calibri", 14))
-        self.ValueEntry.place(x=200, y=150)
+        self.ValueEntry.place(x=200, y=200)
 
         #Concept
         self.ConceptLabel = Label(self.ExpenseFrame, text="Concepto:", bg="Purple", fg="White", font=("Calibri", 14))
-        self.ConceptLabel.place(x=50, y=200)
+        self.ConceptLabel.place(x=50, y=250)
         
         self.ConceptEntry = Entry(self.ExpenseFrame, font=("Calibri", 14))
-        self.ConceptEntry.place(x=200, y=200)
+        self.ConceptEntry.place(x=200, y=250)
 
         #Observations
         self.ObservationsLabel = Label(self.ExpenseFrame, text="Observaciones:", bg="Purple", fg="White", font=("Calibri", 14))
-        self.ObservationsLabel.place(x=50, y=250)
+        self.ObservationsLabel.place(x=50, y=300)
         
         self.ObservationsText = Text(self.ExpenseFrame, font=("Calibri", 14), height=3, width=20, xscrollcommand=TRUE)
-        self.ObservationsText.place(x=200, y=250)
+        self.ObservationsText.place(x=200, y=300)
 
         #Button
         self.OKButton = Button(self.ExpenseFrame, text="Introducir Gasto", font=("Calibri", 14), height=2, width=20)
         self.OKButton.bind("<Button-1>",self.Send)
-        self.OKButton.place(x=225, y=360, anchor=N)
+        self.OKButton.place(x=225, y=410, anchor=N)
 
         #Error 
         self.ErrorLabel = Label(self.ExpenseFrame, text="Parámetros Incorrectos", bg="Purple", fg="Purple", font=("Calibri", 14))
-        self.ErrorLabel.place(x=225, y=410, anchor=N)
+        self.ErrorLabel.place(x=225, y=460, anchor=N)
 
     #Action of the button
     def Send(self, event):
